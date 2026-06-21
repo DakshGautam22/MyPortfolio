ca9e92e5658eb12deef53a55a3aec230301687e1
Daksh Gautam <dakshgautam6456@gmail.com>
2026-06-21 19:05:16 +0530

    Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://dakshgautam22.github.io/MyPortfolio/', // Chae this if you are using a custom domain or a user page (dakshgautam22.github.io)
 })
